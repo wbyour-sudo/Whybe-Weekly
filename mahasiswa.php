db679d8f421e714e0b093626eca08f32c70ad508
wbyour-sudo <wbyour@gmail.com>
2026-06-25 11:01:48 +0700

    php update

--- mahasiswa.php
+++ mahasiswa.php
@@ -1,53 +1,94 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+    $koneksi = mysqli_connect("localhost", "root", "", "faaweekly");
 
+    $query = "SELECT * FROM mahasiswa";
+
+    $result = mysqli_query($koneksi, $query);
+?>
+
+<!DOCTYPE html>
+<html lang="id">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="asset/style.css">
+    <title>Data Mahasiswa - TI UNIMUS</title>
+
+    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/style.css">
 </head>
 
-<body>
-    <h1 align="center">WEBTI | YB</h1>
-    <table border="1" cellspacing="0" cellpadding="10px" align="center">
-        <tr>
-            <td><a href="index.php">home</a></td>
-            <td><a href="profil.php">profile</a></td>
-            <td><a href="kontak.php">contact</a></td>
-            <td><a href="mahasiswa.php">data</a></td>
-        </tr>
-    </table>
-    <h1>data mahasiswa</h1>
-    <table border="1" cellpadding="10px" cellspacing="0" 
-        <tr>
-            <th>id</th>
-            <th>nama</th>
-            <th>nim</th>
-            <th>prodi</th>
-            <th>email</th>
-            <th>no_hp</th>
-            <th>foto</th>
-            <th>aksi</th>
-        </tr>
-        <tr>
-        <a href="tambah.php"><button>Tambah data</button></a>
-        <td>1</td>
-        <td>Yanuar Eka Arifiyanto</td>
-        <td>13242520002</td>
-        <td>Teknologi Informasi</td>
-        <td>wbyour@gmail.com</td>
-        <td>08888888888</td>
-        <td>
-            <img src="/asset/urbae.png" alt="" width='100px'></td>
-        <td>
-            <a href="edit.php"><button>Edit</button></a> <b>|</b> 
-            <a href="delete.php"><button>hapus</button></a>
-        </td>
-        </tr>
-       
-    </table>
+<body class="mahasiswa-page">
+
+    <header>
+        <h1>WEB TI UNIMUS</h1>
+
+        <nav>
+            <a href="index.php">Home</a>
+            <a href="profil.php">About</a>
+            <a href="kontak.php">Contact</a>
+            <a href="mahasiswa.php">Data Mahasiswa</a>
+        </nav>
+
+    </header>
+
+    <main>
+        <h2>Data Mahasiswa</h2>
+
+            <a href="tambahdata.php" class="btn-tambah">
+            + Tambah Data
+            </a>
+
+        <br><br>
+
+        <table border="1" cellpadding="10" cellspacing="0">
+            <tr>
+                <th>No</th>
+                <th>Nama</th>
+                <th>Prodi</th>
+                <th>Email</th>
+                <th>No HP</th>
+                <th>Foto</th>
+                <th>Aksi</th>
+            </tr>
+            <?php
+                while ($mhs = mysqli_fetch_assoc($result))
+                {
+            ?>
+            <tr>
+                <td>1</td>
+                <td>Yanuar Eka Arifiyanto</td>
+                <td>Teknologi Informasi</td>
+                <td>samlekab@gmail.com</td>
+                <td>08957062620800</td>
+                <td>
+                    <img src="assets/image/urbae.png"
+                         alt="Yanuar Eka"
+                         width="100">
+                </td>
+                <td>
+                    <a href="editdata.php?id=1" class="btn-edit">
+                    Edit
+                    </a>
+
+                    <a href="hapusdata.php?id=1" class="btn-hapus"
+                    onclick="return confirm('Yakin ingin menghapus data ini?')">
+                    Hapus
+                    </a>
+                </td>
+            </tr>
+
+            <?php
+                };
+            ?>
+
+        </table>
+    </main>
+
+    <br><br>
+
+    <footer align="center">
+        <p>&copy; 2026 TI UNIMUS</p>
+    </footer>
 
 </body>
-
 </html>
